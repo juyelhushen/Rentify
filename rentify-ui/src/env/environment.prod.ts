8db6727e2dbe2c561e6d0bee2b2e62a8d0cc4b22
Juyel <juyelhushen@gmail.com>
2024-05-27 18:31:27 +0530

    db details has been changes

--- rentify-ui/src/env/environment.prod.ts
+++ rentify-ui/src/env/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl : 'Rentify-env.eba-sppku93h.ap-south-1.elasticbeanstalk.com'
+    apiUrl : 'http://presidio-rentify.ap-south-1.elasticbeanstalk.com'
   };
-  
--- rentify-ui/src/env/environment.ts
+++ rentify-ui/src/env/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl : 'Rentify-env.eba-sppku93h.ap-south-1.elasticbeanstalk.com'
+    apiUrl : 'http://presidio-rentify.ap-south-1.elasticbeanstalk.com'
   };
-  
